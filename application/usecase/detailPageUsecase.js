3cef54d6fed8ce17a3cfcbec8854b6fa159f6d4f
yuki-misumi-sbisecsol <yuki.misumi@sbisecsol.com>
2021-01-14 22:45:37 +0900

    added TODO comments

--- application/usecase/detailPageUsecase.js
+++ application/usecase/detailPageUsecase.js
@@ -3,12 +3,14 @@ const trainingService = require('../service/trainingService');
 const achievementService = require('../service/achievementService');
 
 class DetailPageUsecase {
+    // TODO: add year and month arguments
     async createTrainingDetailData(trainingId) {
         const trainingPromise = trainingService.findById(trainingId);
         const achievementsPromise = achievementService.find({ trainingId });
         const [training, achievements] = await Promise.all([trainingPromise, achievementsPromise]);
 
         const achievementsAggregate = new AchievementsAggregate(training, achievements);
+        // TODO: change janAchievements:.. with relevant year and month
         return { training, totalVelocity: achievementsAggregate.sumUpVelocities(), janAchievements: achievementsAggregate.extractAchievementsByMonth('2021', '01')};
     }
 }
