73e0f51030cfc76c9a7195545c51c8f3d57d07e6
yuki-misumi-sbisecsol <yuki.misumi@sbisecsol.com>
2021-02-07 22:54:50 +0900

    added login functionality

--- app.js
+++ app.js
@@ -1,23 +1,67 @@
-const path = require('path');
-const express = require('express');
+require("dotenv").config();
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const path = require("path");
+const express = require("express");
 const app = express();
 
-const logger = require('./logger');
+const logger = require("./logger");
 
 const PORT = process.env.PORT || 8000;
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        return done(null, profile);
+      });
+    }
+  )
+);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+      console.log('inside req.isauthenticated')
+    return next();
+  }
+  console.log('before redirect to auth/login', req.path)
+  res.redirect("/auth/login");
+}
+
 app.use(express.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-const indexRouter = require('./routes/indexRouter');
-const trainingRouter = require('./routes/trainingRouter');
-const achievementRouter = require('./routes/achievementRouter');
+const indexRouter = require("./routes/indexRouter");
+const trainingRouter = require("./routes/trainingRouter");
+const achievementRouter = require("./routes/achievementRouter");
+const authRouter = require("./routes/authRouter");
 
-app.use('/', indexRouter);
-app.use('/training', trainingRouter);
-app.use('/achievement', achievementRouter);
+app.use("/index", ensureAuthenticated, indexRouter);
+app.use("/training", ensureAuthenticated, trainingRouter);
+app.use("/achievement", ensureAuthenticated, achievementRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
-    logger.info('listening on %s', PORT);
+  logger.info("listening on %s", PORT);
 });
